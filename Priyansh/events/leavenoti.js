b6a78d1d4abc801363bf9baa58305fe35f3fb283
sameerchamar5 <sameerk63625@gmail.com>
2026-04-29 09:21:30 +0530

    Update leavenoti.js

--- Priyansh/events/leavenoti.js
+++ Priyansh/events/leavenoti.js
@@ -25,7 +25,9 @@ module.exports.run = async function({ api, event, Users, Threads }) {
 	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
   
   var link = [  
-"https://i.imgur.com/HpE7OhS.mp4",
+"https://i.imgur.com/RmEPAht.gif",
+"https://i.imgur.com/ZH5U3Vx.gif",	  
+"https://i.imgur.com/JDJCNsL.gif",
   ];
   var callback = () => api.sendMessage({ body: msg, attachment: fs.createReadStream(__dirname + "/cache/leiamnashO.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/leiamnashO.jpg"));
     return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/leiamnashO.jpg")).on("close", () => callback());
